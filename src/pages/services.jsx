4ef0a2b1b595b55600ae4c1296e992daf525bcea
yigitaksoy <yigitaksoy@gmail.com>
2023-11-16 19:01:57 +0100

    Add infinite marquee and styles to services section

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+"use client";
+
+import { Container } from "@/components/Container";
+import InfiniteMarquee from "@/components/InfiniteMarquee";
+
+const texts = [
+  [
+    "Web Design",
+    "E-commerce",
+    "Web Development",
+    "Wordpress",
+    "Maintanence",
+    "Shopify",
+    "Backend",
+  ],
+  [
+    "UX/UI Design",
+    "Optimization",
+    "Redesigns",
+    "Functionality",
+    "Landing Pages",
+    "Applications",
+  ],
+];
 
 const services = () => {
   return (
-    <div className="h-screen flex flex-col text-white justify-center text-5xl font-heavy">
-      <p className="text-enter">Services</p>
-    </div>
+    <section id="hero" className="h-screen">
+      <Container className="mt-36 sm:mt-32 md:mt-50">
+        <div className="max-w-7xl">
+          <h1 className="font-display text-center text-4xl mb-10 font-heavy tracking-tight text-white [text-wrap:balance] sm:text-6xl">
+            Services
+          </h1>
+        </div>
+        <InfiniteMarquee
+          className="text-white text-5xl font-heavy md:text-7xl"
+          texts={texts}
+          hollow
+        />
+      </Container>
+    </section>
   );
 };
 
