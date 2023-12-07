05bd8248fef52fe617e23c08e1423a5a3b412a35
yigitaksoy <yigitaksoy@gmail.com>
2023-12-07 10:00:45 +0100

    Add section component

---
+++ src/components/Section.jsx
@@ -0,0 +1,33 @@
+import { Container } from "@/components/Container";
+import { FadeIn } from "@/components/FadeIn";
+import { StylizedImage } from "@/components/StylizedImage";
+
+export default function Section({ title, image, children }) {
+  return (
+    <Container className="group/section [counter-increment:section]">
+      <div className="lg:flex lg:items-center lg:justify-end lg:gap-x-2 lg:group-even/section:justify-start xl:gap-x-20">
+        <div className="flex justify-center">
+          <FadeIn className="w-[33.75rem] flex-none lg:w-[40rem]">
+            <StylizedImage
+              {...image}
+              sizes="(min-width: 1024px) 41rem, 31rem"
+              className="justify-center lg:justify-end lg:group-even/section:justify-start opacity-80"
+            />
+          </FadeIn>
+        </div>
+        <div className="mt-12 lg:mt-0 lg:pt-16 lg:w-[37rem] lg:flex-none lg:group-even/section:order-first">
+          <FadeIn>
+            <div
+              className="font-display text-base font-semibold italic before:text-neutral-300 before:content-['/_'] after:text-white after:content-[counter(section,decimal-leading-zero)]"
+              aria-hidden="true"
+            />
+            <h2 className="mt-2 font-display text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              {title}
+            </h2>
+            <div className="mt-6">{children}</div>
+          </FadeIn>
+        </div>
+      </div>
+    </Container>
+  );
+}
