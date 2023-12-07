1184e0fbfb9a817ca81cb5122c64ddebee5b5a7a
yigitaksoy <yigitaksoy@gmail.com>
2023-12-07 10:02:19 +0100

    Add service details data

---
+++ src/data/services.js
@@ -0,0 +1,45 @@
+import webDesignImg from "@/images/web-design.png";
+import webDevImg from "@/images/web-development.png";
+
+export const data = [
+  {
+    id: 1,
+    title: "Web Design",
+    image: webDesignImg,
+    paragraph1:
+      "At House 58, we understand that a website is the digital face of your business, setting the stage for customer interaction and brand perception. Our web development service begins with a deep understanding of your business goals and audience needs. We leverage cutting-edge tools and frameworks to ensure your website not only looks stunning but also functions flawlessly.",
+    paragraph2:
+      "We focus on creating intuitive user interfaces, integrating engaging content, and optimizing for mobile responsiveness, ensuring that your website stands out in today's competitive digital landscape. We prioritize user experience, creating websites that are easy to navigate and visually appealing.",
+    paragraph3:
+      "Finally, our commitment to your website's success extends beyond its launch. We provide ongoing support and maintenance, ensuring your site remains up-to-date with the latest web standards and technologies. Whether it's implementing new features, updating content, or optimizing for search engines, House 58 is dedicated to ensuring your website continues to drive business growth and deliver exceptional user experiences.",
+    tags: [
+      "Redesigns",
+      "Frontend",
+      "Branding",
+      "UI/UX Design",
+      "Landing Pages",
+      "Interactive Design",
+      "Wordpress",
+    ],
+  },
+  {
+    id: 2,
+    title: "Web Development",
+    image: webDevImg,
+    paragraph1:
+      "In the dynamic world of web development, House 58 provides comprehensive Web Development services. We're dedicated to building solutions that not only meet but exceed the digital needs of your business. Our approach is grounded in creating web applications that are both highly functional and aesthetically appealing, ensuring your digital presence resonates with your audience.",
+    paragraph2:
+      "Recognizing the growing importance of AI and cloud technologies in the digital landscape, we at House 58 offer integrated solutions that enhance your web platform with these advanced technologies. Our expertise lies in skillfully implementing and leveraging AI and cloud capabilities to deliver web applications that are scalable, secure, and at the forefront of digital innovation.",
+    paragraph3:
+      "At House 58, we understand that each business has unique needs. Our commitment lies in delivering tailored web development services – from crafting user-friendly interfaces and robust backend systems to efficient data management. We strive to build a web presence that not only reflects your brand's identity but also supports its growth. Partner with us to create a web solution that is contemporary, adaptable, and ready to evolve alongside your business.",
+    tags: [
+      "Optimization",
+      "Applications",
+      "Cloud Solutions",
+      "CRM",
+      "Backend",
+      "Custom Solutions",
+      "AI Integration",
+    ],
+  },
+];
