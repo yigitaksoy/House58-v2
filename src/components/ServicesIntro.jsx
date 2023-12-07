67414fb7e7b24cf7bbcf6dae6f97875207b39869
yigitaksoy <yigitaksoy@gmail.com>
2023-12-07 10:01:59 +0100

    Add fade in component to services intro

--- src/components/ServicesIntro.jsx
+++ src/components/ServicesIntro.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Container } from "@/components/Container";
+import { FadeIn } from "./FadeIn";
 import InfiniteMarquee from "@/components/InfiniteMarquee";
 
 const texts = [
@@ -26,14 +27,14 @@ const texts = [
 
 const ServicesIntro = () => {
   return (
-    <section id="services-intro" className="h-screen">
+    <section id="services-intro" className="h-auto">
       <Container className="mt-36 sm:mt-32 md:mt-60">
-        <div className="max-w-2xl">
-          <h1 className="font-display text-4xl mb-14 font-heavy tracking-tight text-white [text-wrap:balance] sm:text-5xl leading-5">
+        <FadeIn className="max-w-2xl pt-14">
+          <h1 className="text-4xl font-heavy tracking-tight text-white [text-wrap:balance] sm:text-6xl animate">
             Designing standout solutions using sound strategic thinking.
           </h1>
-        </div>
-        <div className="mt-10">
+        </FadeIn>
+        <div className="mt-36">
           <InfiniteMarquee
             className="text-white md:text-6xl text-3xl md:py-5 py-3 font-heavy"
             texts={texts}
