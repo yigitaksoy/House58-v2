953b7c5408b8e6a8025fe9123bd928dd03b9bd46
yigitaksoy <yigitaksoy@gmail.com>
2023-12-10 21:31:07 +0100

    Add opacity and mouse-over animations to services heading and marquee

--- src/components/ServicesIntro.jsx
+++ src/components/ServicesIntro.jsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { useRef } from "react";
 import { Container } from "@/components/Container";
 import { FadeIn } from "./FadeIn";
+import { useIsomorphicLayoutEffect } from "@/helpers/useIsomorphicEffect";
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import InfiniteMarquee from "@/components/InfiniteMarquee";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const texts = [
   [
@@ -26,17 +32,81 @@ const texts = [
 ];
 
 const ServicesIntro = () => {
+  const main = useRef();
+  const h1Ref = useRef();
+
+  useIsomorphicLayoutEffect(() => {
+    let animate = gsap.utils.selector(main.current)(".animate");
+
+    let mm = gsap.matchMedia();
+
+    mm.add("(min-width: 800px)", () => {
+      gsap.to(animate, {
+        y: -50,
+        opacity: 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: animate,
+          start: "40% 40%",
+          end: "bottom 5%",
+          scrub: true,
+        },
+      });
+    });
+
+    mm.add("(max-width: 799px)", () => {
+      gsap.to(animate, {
+        opacity: 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: animate,
+          start: "bottom 10%",
+          end: "top 10%",
+          scrub: 1,
+          reverse: true,
+          smoothChildTiming: true,
+        },
+      });
+    });
+
+    // Mouse move effect
+    const moveText = (event) => {
+      const { clientX, clientY } = event;
+      const { left, top, width, height } = main.current.getBoundingClientRect();
+      const x = ((clientX - left - width / 2) / width) * 8; // strength of the effect
+      const y = ((clientY - top - height / 2) / height) * 15;
+
+      gsap.to([h1Ref.current], {
+        x,
+        y,
+        duration: 0.5,
+        ease: "power1.out",
+      });
+    };
+
+    main.current.addEventListener("mousemove", moveText);
+
+    // Cleanup
+    return () => {
+      mm.revert();
+      main.current.removeEventListener("mousemove", moveText);
+    };
+  }, []);
+
   return (
-    <section id="services-intro" className="h-auto">
+    <section id="services-intro" className="h-auto" ref={main}>
       <Container className="mt-36 sm:mt-32 md:mt-60">
         <FadeIn className="max-w-2xl pt-14">
-          <h1 className="text-4xl font-heavy tracking-tight text-white [text-wrap:balance] sm:text-6xl animate">
+          <h1
+            ref={h1Ref}
+            className="text-4xl font-heavy tracking-tight text-white [text-wrap:balance] sm:text-6xl md:leading-[4.5rem] animate"
+          >
             Designing standout solutions using sound strategic thinking.
           </h1>
         </FadeIn>
-        <div className="mt-36">
+        <div className="mt-32">
           <InfiniteMarquee
-            className="text-white md:text-6xl text-3xl md:py-5 py-3 font-heavy"
+            className="text-white md:text-6xl text-3xl md:py-5 py-3 font-heavy animate"
             texts={texts}
             hollow
           />
