0b1475a9eed6d4064bcb90d566b19f80d360d8a2
yigitaksoy <yigitaksoy@gmail.com>
2023-12-13 00:29:27 +0100

    Add weather codes and data

---
+++ src/data/weather-data.js
@@ -0,0 +1,39 @@
+import cloudsIcon from "@/images/weather-icons/clouds.png";
+import cloudWithSnowIcon from "@/images/weather-icons/cloud-with-snow.png";
+import sunIcon from "@/images/weather-icons/sun.png";
+import rainIcon from "@/images/weather-icons/cloud-with-rain.png";
+import showersIcon from "@/images/weather-icons/cloud-with-strong-rain.png";
+import sunWithCloudsIcon from "@/images/weather-icons/sun-with-clouds.png";
+import cloudWithThunderIcon from "@/images/weather-icons/cloud-with-thunder.png";
+import cloudWithStrongThunderIcon from "@/images/weather-icons/cloud-with-strong-thunder.png";
+
+export const weatherIcons = {
+  0: sunIcon,
+  1: sunWithCloudsIcon,
+  2: sunWithCloudsIcon,
+  3: sunWithCloudsIcon,
+  45: cloudsIcon,
+  48: cloudsIcon,
+  51: rainIcon,
+  53: rainIcon,
+  55: rainIcon,
+  56: rainIcon,
+  57: rainIcon,
+  61: rainIcon,
+  63: rainIcon,
+  65: rainIcon,
+  66: cloudWithSnowIcon,
+  67: cloudWithSnowIcon,
+  71: cloudWithSnowIcon,
+  73: cloudWithSnowIcon,
+  75: cloudWithSnowIcon,
+  77: cloudWithSnowIcon,
+  85: cloudWithSnowIcon,
+  86: cloudWithSnowIcon,
+  80: showersIcon,
+  81: showersIcon,
+  82: showersIcon,
+  95: cloudWithThunderIcon,
+  96: cloudWithStrongThunderIcon,
+  99: cloudWithStrongThunderIcon,
+};
