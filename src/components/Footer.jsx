8d1a13540589592ab671a267087dcabcf589522a
yigitaksoy <yigitaksoy@gmail.com>
2023-12-13 02:18:35 +0100

    Add social icons and email link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import Link from "next/link";
 import Magnetic from "@/components/Magnetic";
 import { weatherIcons } from "@/data/weather-data";
+import { SiLinkedin } from "react-icons/si";
+import { TbMailFast } from "react-icons/tb";
 
 const Footer = () => {
   const [weatherData, setWeatherData] = useState(null);
@@ -51,33 +53,48 @@ const Footer = () => {
       <div className="border-t-4 border-house-black"></div>
       <div className="absolute bottom-0 w-full">
         <div className="border-t border-house-black bg-house-black pb-12 xl:pb-20">
-          <div className="mx-auto max-w-screen-section px-4 sm:px-8 lg:px-12 lg:text-left">
+          <div className="mx-auto max-w-screen-section px-4 sm:px-8 lg:px-24 lg:text-left">
             <div className="flex flex-col pt-12 xl:flex-row xl:items-center xl:justify-between xl:pt-20">
-              <div className="order-2 mt-8 inline-flex gap-4 xl:mt-0">
+              <div className="order-2 mt-8 flex gap-4 xl:mt-0">
                 {/* Social media icons */}
                 <Magnetic>
-                  <Link href="/" className="group">
-                    <span className="text-white duration-300 bg-bottom bg-gradient-to-r from-house-bluelight to-house-bluelight bg-[length:0%_2px] bg-no-repeat transform transition-all ease-out group-hover:bg-[length:100%_1.5px] font-thin text-sm">
-                      Privacy
-                    </span>
+                  <Link
+                    href="https://www.linkedin.com/company/house-58-digital/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center text-house-whitewarm"
+                  >
+                    <SiLinkedin className="text-house-bluelight" />
+                    <span className="text-sm font-thin ml-1">Linkedin</span>
                   </Link>
+                </Magnetic>
+                <Magnetic>
+                  <a
+                    href="mailto:hello@house58.nl?subject=Lets%20Start%20a%20Project"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center text-house-whitewarm"
+                  >
+                    <TbMailFast className="text-house-bluelight w-6 h-6" />
+                    <span className="text-sm font-thin ml-1">Email</span>
+                  </a>
                 </Magnetic>
               </div>
               <div className="order-2 inline-flex text-sm font-thin text-house-whitewarm">
                 {weatherData && (
                   <div className="weather-widget flex">
-                    <div className="mr-2">AMS</div>
-                    <div className="mr-2">
+                    <div className="mr-3">AMS</div>
+                    <div className="mr-3">{weatherData.temperature}°C</div>
+                    <div className="mr-3">
                       {currentHours}
                       <span className="animate-pulse">:</span>
                       {currentMinutes}
                     </div>
-                    <div className="mr-2">{weatherData.temperature}°C</div>
                     <div>
                       <Image
                         src={weatherIcons[weatherData.weathercode]}
                         alt="weather-icon"
-                        className="ml-2 mt-[3.5px] h-[15px] w-[16px]"
+                        className=" mt-[3.5px] h-[15.2px] w-[16px]"
                       />
                     </div>
                   </div>
