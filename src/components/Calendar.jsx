4382fbcc14553f14b77909d9532048238bac1d6e
yigitaksoy <yigitaksoy@gmail.com>
2023-12-31 15:06:31 +0100

    Add calender widget for meeting scheduling

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { useIsomorphicLayoutEffect } from "@/helpers/useIsomorphicEffect";
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { getCalApi } from "@calcom/embed-react";
+import { CiCalendar } from "react-icons/ci";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Calendar() {
+  const buttonRef = useRef(null);
+
+  useIsomorphicLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const animateButton = buttonRef.current;
+
+      if (animateButton) {
+        gsap.set(animateButton, { opacity: 0, scale: 0.5 });
+
+        ScrollTrigger.create({
+          trigger: document.body,
+          start: "top+=200 top",
+          onEnter: () =>
+            gsap.to(animateButton, {
+              opacity: 1,
+              scale: 1,
+              duration: 0.1,
+              ease: "back.inOut(1.7)",
+            }),
+          onLeave: () =>
+            gsap.to(animateButton, {
+              opacity: 0,
+              scale: 0.5,
+              duration: 0.1,
+              ease: "back.inOut(1.7)",
+            }),
+          onEnterBack: () =>
+            gsap.to(animateButton, {
+              opacity: 1,
+              scale: 1,
+              duration: 0.1,
+              ease: "back.inOut(1.7)",
+            }),
+          onLeaveBack: () =>
+            gsap.to(animateButton, {
+              opacity: 0,
+              scale: 0.5,
+              duration: 0.1,
+              ease: "back.inOut(1.7)",
+            }),
+        });
+      }
+    });
+    // Cleanup if the component unmounts
+    return () => ctx.revert();
+  }, []);
+
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi();
+      cal("ui", {
+        theme: "dark",
+        styles: {
+          branding: { brandColor: "#100f0f" },
+        },
+      });
+    })();
+  }, []);
+
+  return (
+    <div>
+      <button
+        ref={buttonRef} // Attach the ref here
+        data-cal-link="house58/30"
+        className=" z-[999999999999] fixed md:bottom-4 bottom-4 md:right-6 right-4 flex h-11 origin-center bg-house-bluelight font-objektiv transform cursor-pointer items-center text-sm hover:border-house-bluelight
+        hover:outline-1 rounded-full py-5 px-6 outline-none drop-shadow-lg transition duration-300 hover:bg-house-black hover:text-white book-a-call"
+      >
+        <CiCalendar className="mr-2 h-5 w-5" />
+        Book a meeting
+      </button>
+    </div>
+  );
+}
