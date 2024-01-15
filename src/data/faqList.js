9d144c705f56be8e2be8bd47ffb58245a6554a19
yigitaksoy <yigitaksoy@gmail.com>
2024-01-16 00:54:10 +0300

    Add faq data

---
+++ src/data/faqList.js
@@ -0,0 +1,51 @@
+export const faqList = [
+  {
+    id: 1,
+    question: "Is there a minimum starting budget?",
+    answer:
+      "We don’t require monthly/weekly minimums. We will evaluate your project individually based on the scope of work, goals, and deadlines. If the scope of work doesn’t match your budget allocation, we will always advise you on workarounds.",
+  },
+  {
+    id: 2,
+    question: "I have a tight timeline. Will you take my project?",
+    answer:
+      "We customize timelines based on each project's unique needs, always leaving room for high-quality design and revisions. While we can adjust for urgency, extremely tight deadlines often affect quality, which we're not willing to compromise. We never accept projects with same-day delivery, and we don't take web design projects due in less than 30 days, no matter how small they are.",
+  },
+  {
+    id: 4,
+    question: "Is there a legal binding between us?",
+    answer:
+      "Every project comes with a detailed agreement covering scope, payment and service terms, as well as copyright, licensing, and warranties. If an NDA is required, we're more than happy to sign.",
+  },
+  {
+    id: 3,
+    question: "Do I have any responsibilities?",
+    answer:
+      "We thrive on close collaborations and value being an extension of your team. The more you can share about your brand and project, the better. Even minor details can be game-changers. While we respect your timelines, speedy project completion hinges on your timely feedback. We ask for responses within 3 business days to keep things moving.",
+  },
+  {
+    id: 7,
+    question:
+      "Can you integrate advanced features into my website, like AI or CRM systems?",
+    answer:
+      "Certainly. Our expertise in web development includes the integration of advanced features such as AI functionalities and CRM systems. We focus on enhancing your website's capabilities to meet the evolving demands of the digital world. Whether it's improving user interaction through AI or streamlining customer management with a CRM system, we ensure your website is not only cutting-edge but also aligns perfectly with your operational needs and business goals.",
+  },
+  {
+    id: 8,
+    question: "What platforms do you use for e-commerce website development?",
+    answer:
+      "For e-commerce websites, we specialize in both custom solutions and popular platforms like WooCommerce and Shopify. Our choice of platform is guided by your specific business needs, scalability, and functionality requirements. We ensure a seamless integration of the e-commerce platform with your website, providing an efficient, secure, and user-friendly shopping experience for your customers.",
+  },
+  {
+    id: 6,
+    question: "Do you provide support after the project is completed?",
+    answer:
+      "Absolutely. We offer a variety of post-launch support services to ensure your website runs smoothly. This includes technical assistance, security updates, and minor tweaks. Depending on your needs, we can discuss ongoing maintenance packages or provide support on an as-needed basis.",
+  },
+  {
+    id: 5,
+    question: "What if I will not be satisfied with the results?",
+    answer:
+      "If you're not completely satisfied with the end result, rest assured we have built-in opportunities for revisions during each stage of the design process. We offer two initial rounds of edits to align closely with your vision. If further changes are needed, additional revisions can be made at an hourly rate, always with your prior approval. Our aim is to make sure you're pleased with the final design, and we're committed to working with you until it meets your expectations.",
+  },
+];
