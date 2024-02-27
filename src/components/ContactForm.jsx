e1dd4b44e62602e3f72b379bc73b852546794fa2
yigitaksoy <yigitaksoy@gmail.com>
2024-02-27 02:23:55 +0100

    Add form submit status messages

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -240,60 +240,70 @@ const ContactForm = () => {
               How can we help you? *
             </label>
           </div>
-          <button
-            className="flex group relative w-40 items-center justify-between rounded-lg py-4 bg-black border-2 border-house-black mt-2 mb-4 font-bold leading-none text-house-whitewarm transition-transform duration-500 hover:bg-house-black"
-            type="submit"
-            disabled={formSubmitted}
-          >
-            {!sending && !formSubmitted && !error && (
-              <>
-                <span
-                  ref={submitTextRef}
-                  className="font-bold ml-5 group-hover:text-house-whitewarm group-hover:-translate-x-1 transition-transform duration-500"
-                >
-                  Submit
-                </span>
-                <div
-                  ref={arrowRef}
-                  className="absolute group right-[25%] translate-x-1/2 top-1/2 -translate-y-1/2 w-10 text-house-whitewarm overflow-hidden flex items-center justify-center"
-                >
-                  <span className="group-hover:text-house-green group-hover:rotate-45 group-hover:translate-x-2 group-hover:animate-pulse transition-transform duration-500">
-                    <GoArrowUpRight className="text-2xl" />
-                  </span>
-                </div>
-              </>
-            )}
-            {sending && (
-              <div className="py-2">
-                <div
-                  ref={spinnerRef}
-                  className="absolute inset-0 flex justify-center items-center"
-                >
-                  <ImSpinner2 className="w-6 h-6 text-house-bluelight fill-house-bluelight animate-spin" />
-                </div>
-              </div>
-            )}
-            {formSubmitted && (
-              <div className="py-2">
-                <div
-                  ref={successIconRef}
-                  className="absolute inset-0 flex justify-center items-center"
-                >
-                  <FaCheckCircle className="w-6 h-6 text-house-black fill-house-green " />
-                </div>
-              </div>
-            )}
-            {error && !sending && !formSubmitted && (
-              <div className="py-2">
-                <div
-                  ref={errorIconRef}
-                  className="absolute inset-0 flex justify-center items-center"
-                >
-                  <BiSolidError className="w-6 h-6 text-yellow-400 " />
-                </div>
-              </div>
-            )}
-          </button>
+          <div className="mt-2 grid grid-cols-2 gap-5">
+            <div>
+              <button
+                className="flex group relative w-40 items-center justify-between rounded-lg py-4 bg-black border-2 border-house-black mt-2 mb-4 font-bold leading-none text-house-whitewarm transition-transform duration-500 hover:bg-house-black"
+                type="submit"
+                disabled={formSubmitted}
+              >
+                {!sending && !formSubmitted && !error && (
+                  <>
+                    <span
+                      ref={submitTextRef}
+                      className="font-bold ml-5 group-hover:text-house-whitewarm group-hover:-translate-x-1 transition-transform duration-500"
+                    >
+                      Submit
+                    </span>
+                    <div
+                      ref={arrowRef}
+                      className="absolute group right-[25%] translate-x-1/2 top-1/2 -translate-y-1/2 w-10 text-house-whitewarm overflow-hidden flex items-center justify-center"
+                    >
+                      <span className="group-hover:text-house-green group-hover:rotate-45 group-hover:translate-x-2 group-hover:animate-pulse transition-transform duration-500">
+                        <GoArrowUpRight className="text-2xl" />
+                      </span>
+                    </div>
+                  </>
+                )}
+                {sending && (
+                  <div className="py-2">
+                    <div
+                      ref={spinnerRef}
+                      className="absolute inset-0 flex justify-center items-center"
+                    >
+                      <ImSpinner2 className="w-6 h-6 text-house-bluelight fill-house-bluelight animate-spin" />
+                    </div>
+                  </div>
+                )}
+                {formSubmitted && (
+                  <div className="py-2">
+                    <div
+                      ref={successIconRef}
+                      className="absolute inset-0 flex justify-center items-center"
+                    >
+                      <FaCheckCircle className="w-6 h-6 text-house-black fill-house-green " />
+                    </div>
+                  </div>
+                )}
+                {error && !sending && !formSubmitted && (
+                  <div className="py-2">
+                    <div
+                      ref={errorIconRef}
+                      className="absolute inset-0 flex justify-center items-center"
+                    >
+                      <BiSolidError className="w-6 h-6 text-yellow-400 " />
+                    </div>
+                  </div>
+                )}
+              </button>
+            </div>
+            <div className="grid items-center justify-center">
+              {formSubmitted && <p className="text-sm font-bold">Thank you!</p>}
+              {error && !sending && !formSubmitted && (
+                <p className="text-sm font-bold">Please try again later.</p>
+              )}
+            </div>
+          </div>
         </form>
       </div>
     </div>
