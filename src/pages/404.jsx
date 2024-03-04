f1d22a8ed578eed13b0c44f9ff161979ebdd3b8a
yigitaksoy <yigitaksoy@gmail.com>
2024-03-04 16:14:21 +0100

    Add custom 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,10 @@
+export default function Custom404() {
+  return (
+    <div className="text-white h-screen items-center grid justify-center">
+      <h1 className="text-center text-sm">
+        <span className="text-xl ">404</span>
+        <span className="mx-5 text-xl">|</span>This page could not be found.
+      </h1>
+    </div>
+  );
+}
