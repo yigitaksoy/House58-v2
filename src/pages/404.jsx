9c5fbaa268b37fe17e38dc1f3d37450d8033a9cd
yigitaksoy <yigitaksoy@gmail.com>
2024-03-05 14:47:43 +0100

    Reduce 404 message font size

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,7 +3,7 @@ export default function Custom404() {
     <div className="text-white h-screen items-center grid justify-center">
       <h1 className="text-center text-sm">
         <span className="text-xl ">404</span>
-        <span className="mx-5 text-xl">|</span>This page could not be found.
+        <span className="mx-5 text-lg">|</span>This page could not be found.
       </h1>
     </div>
   );
